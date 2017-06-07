ba5bbf722dbe8768e524df6eaa30fb8a76871a21
edvincandon <edvincandon@gmail.com>
2017-06-07 20:12:33 +0200

    Added default values to $connect

--- src/extend.js
+++ src/extend.js
@@ -17,7 +17,10 @@ export function extendVue(Vue) {
       }
     }
   })
-  Vue.prototype.$connect = function(mapState, mapDispatch) {
+  const defaultMapState = () => ({});
+  const defaultMapDispatch =  dispatch => ({ dispatch });
+
+  Vue.prototype.$connect = function(mapState = defaultMapState, mapDispatch = defaultMapDispatch) {
     const vm = this
     const getMappedState = (state = this.$store.state) => mapState(state)
 
