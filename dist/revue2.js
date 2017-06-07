7ae9e4a64966c132fb361a164e6860f928dddedc
edvincandon <edvincandon@gmail.com>
2017-06-07 19:59:43 +0200

    0.0.4

--- dist/revue2.js
+++ dist/revue2.js
@@ -102,24 +102,9 @@ function extendVue(Vue) {
 }
 
 class Revue {
-  constructor(reduxStore, options) {
-    if (!options.component) {
-      throw new Error('[revue2] - You must provide an entry point component')
-    }
-
+  constructor(reduxStore) {
     this.store = reduxStore;
     this.subscribe = this.subscribe.bind(this);
-
-    const revueInstance = this;
-
-    this.Provider = {
-      render: h => h(options.component),
-      data: function() {
-        return Object.assign({}, {
-          store: revueInstance
-        }, options.data)
-      }
-    };
   }
   subscribe(cb) {
     this.store.subscribe(cb);
