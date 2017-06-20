f721d5252a0aa9450defb8de13234e3c847617db
edvincandon <edvincandon@gmail.com>
2017-06-20 13:16:19 +0200

    expose provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import install from './install'
 import connector from './connector'
+import Provider from './components/Provider'
 
 const isDev = process.env.NODE_ENV !== 'production'
 
@@ -11,3 +12,4 @@ const revux = {
 
 export default revux
 export const connect = connector
+export const provider = Provider
