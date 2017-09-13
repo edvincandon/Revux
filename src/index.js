c259d76875f8f6854a1987119c4ded2983910f0b
edvincandon <edvincandon@gmail.com>
2017-09-13 10:22:50 +0200

    clean up trailing development code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import install from './install'
 import connector from './connector'
-import Provider from './components/Provider'
-
-const isDev = process.env.NODE_ENV !== 'production'
 
 const revux = {
   install(_vue) {
@@ -12,4 +9,3 @@ const revux = {
 
 export default revux
 export const connect = connector
-export const provider = Provider
