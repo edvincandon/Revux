4d290a96f90bd4f1099cb43c318b61de238c76ec
edvincandon <edvincandon@gmail.com>
2017-09-21 10:01:51 +0200

    HOTFIX - expose Provider

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 
 import install from './install'
 import connector from './connector'
+import Provider from './components/Provider'
 
 const revux = {
   install(_vue) {
@@ -14,3 +15,4 @@ const revux = {
 
 export default revux
 export const connect = connector
+export const provider = Provider
